f107ae0e0538b705755f34291a39313073280051
JuanR9910 <juanramos070901@gmail.com>
2021-12-14 22:43:18 -0500

    Created user routes for comments

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,34 @@
 const db = require("../models");
  const router = require("./igdb");
 
+ //Post
+ router.post('/', (req, res) => {
+   db.user.create({
+     content: req.body.content,
+     userId: req.body.userId
+   })
+   .then((post) => {
+     res.redirect('/views/home')
+   })
+   .catch((error) => {
+     res.status(400).render('main/404')
+   })
+ })
+
+// Get
+ router.get('/views/home', (req, res) => {
+   db.user.findAll()
+   .then((comments) => {
+     res.render('views/home', { user: user })
+   })
+   .catch((error) => {
+     res.status(400).render('main/404')
+   })
+ })
+
+
+
+
  router.get('/:id', (req, res) => {
     db.user.findOne({
        where: { id: req.params.id },
@@ -10,7 +38,7 @@ const db = require("../models");
   .then((comment) => {
      if(!comment) throw Error()
      console.log(comment.user)
-     res.render('partials/home'), {user: comment}
+     res.render('views/home'), {user: comment}
   }).catch((error) => {
      console.log(error)
      res.status(400).render('main/404')
