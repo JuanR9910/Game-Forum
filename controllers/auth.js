c8fcc17eabcf29256ef5d6d8034da3e5d7701d89
JuanR9910 <juanramos070901@gmail.com>
2021-12-23 16:57:08 -0500

    nodemon error

--- controllers/auth.js
+++ controllers/auth.js
@@ -53,4 +53,4 @@ router.get('/logout', (req, res)=>{
     res.redirect('/')
 })
 
-module.exports = router
+module.exports = router;
--- controllers/igdb.js
+++ controllers/igdb.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const igdb = require('igdb')
+const axios = require('axios')
 
-app.get('/', (req, res)=>{
+router.get('/', (req, res)=>{
     res.render('home.ejs')
 })
 
-app.get('/review', (req, res)=>{
+router.get('/review', (req, res)=>{
         axios.get(`https://api.rawg.io/api/games/:id?key=${process.env.RAWG_API_KEY}`)
         .then(apiRes => {
             console.log('this is apiRes', apiRes.data)
@@ -17,4 +17,4 @@ app.get('/review', (req, res)=>{
 
 
 
-module.exports = router
+module.exports = router;
